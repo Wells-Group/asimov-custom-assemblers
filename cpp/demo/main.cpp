e746dd33305ba1c9f0e111decd6ddf47633aae30
Igor Baratta <igorbaratta@gmail.com>
2021-06-04 15:41:05 +0100

    Improve c++ assembler (#37)
    * some cpp improvements
    
    * fix version
    
    * get ndofs_cell from basix element
    
    * remove dolfinx dependency
    
    * fix cmake
    
    * Apply suggestions from code review
    
    Co-authored-by: Sarah Roggendorf <33656497+SarahRo@users.noreply.github.com>
    
    * adress some comments
    
    * Fix 1x1x1 case
    
    * improvements in cpp assembler
    
    * fix CI
    
    Co-authored-by: Sarah Roggendorf <33656497+SarahRo@users.noreply.github.com>
    Co-authored-by: Jørgen Dokken <jsd55@cam.ac.uk>

--- cpp/demo/main.cpp
+++ cpp/demo/main.cpp
@@ -1,10 +1,11 @@
-#include "mass.h"
+#include "problem.h"
 #include <basix/finite-element.h>
 #include <basix/quadrature.h>
 #include <dolfinx.h>
 #include <dolfinx/fem/petsc.h>
+#include <dolfinx_cuas/kernels.hpp>
 #include <xtensor/xio.hpp>
-#include <dolfinx_cuas/assemble.hpp>
+
 using namespace dolfinx;
 
 int main(int argc, char* argv[])
@@ -14,51 +15,72 @@ int main(int argc, char* argv[])
 
   MPI_Comm mpi_comm{MPI_COMM_WORLD};
 
-  int mesh_string = 0;
-  std::shared_ptr<mesh::Mesh> mesh;
+  std::shared_ptr<mesh::Mesh> mesh = std::make_shared<mesh::Mesh>(
+      generation::BoxMesh::create(mpi_comm, {{{0.0, 0.0, 0.0}, {1.0, 1.0, 1.0}}}, {50, 50, 50},
+                                  mesh::CellType::tetrahedron, mesh::GhostMode::none));
 
-  switch (mesh_string)
-  {
-  case 0:
-    mesh = std::make_shared<mesh::Mesh>(
-        generation::BoxMesh::create(mpi_comm, {{{0.0, 0.0, 0.0}, {1.0, 1.0, 1.0}}}, {50, 50, 50},
-                                    mesh::CellType::tetrahedron, mesh::GhostMode::none));
-    break;
-  case 1:
-    mesh = std::make_shared<mesh::Mesh>(
-        generation::RectangleMesh::create(mpi_comm, {{{0.0, 0.0}, {1.0, 1.0}}}, {500, 500},
-                                          mesh::CellType::triangle, mesh::GhostMode::none));
-    break;
-  default:
-    dolfinx::fem::CoordinateElement element(dolfinx::mesh::CellType::triangle, 1);
-    xt::xtensor<double, 2> geom{{0.1, 0.}, {1, 0.}, {0., 1}};
-    xt::xtensor<std::int64_t, 2> topo{{0, 1, 2}};
-
-    auto [data, offset] = dolfinx::graph::create_adjacency_data(topo);
-    auto cells = dolfinx::graph::AdjacencyList<std::int64_t>(data, offset);
-    mesh = std::make_shared<mesh::Mesh>(
-        create_mesh(mpi_comm, cells, element, geom, dolfinx::mesh::GhostMode::none));
-  }
+  mesh->topology().create_entity_permutations();
 
   const std::shared_ptr<fem::FunctionSpace>& V
-      = fem::create_functionspace(functionspace_form_mass_a, "u", mesh);
-
-  dolfinx::common::Timer t0("this");
-  auto A_csr = dolfinx_cuas::assemble_nedelec_mass_matrix<1>(V);
-  t0.stop();
+      = fem::create_functionspace(functionspace_form_problem_a, "u", mesh);
 
   // Define variational forms
   auto kappa = std::make_shared<fem::Constant<PetscScalar>>(1.0);
   auto a = std::make_shared<fem::Form<PetscScalar>>(
-      fem::create_form<PetscScalar>(*form_mass_a, {V, V}, {}, {{"kappa", kappa}}, {}));
+      fem::create_form<PetscScalar>(*form_problem_a, {V, V}, {}, {{"kappa", kappa}}, {}));
 
+  // Define active cells
+  const std::int32_t tdim = mesh->topology().dim();
+  const std::int32_t ncells = mesh->topology().index_map(tdim)->size_local();
+  xt::xarray<std::int32_t> active_cells = xt::arange<std::int32_t>(0, ncells);
+
+  // Extract function space data
+  std::shared_ptr<const fem::DofMap> dofmap0 = a->function_spaces().at(0)->dofmap();
+  std::shared_ptr<const fem::DofMap> dofmap1 = a->function_spaces().at(1)->dofmap();
+  const graph::AdjacencyList<std::int32_t>& dofs0 = dofmap0->list();
+  const int bs0 = dofmap0->bs();
+  const graph::AdjacencyList<std::int32_t>& dofs1 = dofmap1->list();
+  const int bs1 = dofmap1->bs();
+  std::vector<bool> bc0;
+  std::vector<bool> bc1;
+
+  // Pack constants and coefficients
+  const std::vector<double> constants = dolfinx::fem::pack_constants(*a);
+  const array2d<double> coeffs = dolfinx::fem::pack_coefficients(*a);
+
+  auto cell_info = mesh->topology().get_cell_permutation_info();
+
+  auto kernel = dolfinx_cuas::generate_kernel("Lagrange", "tetrahedron", Kernel::Stiffness, 1);
+
+  // Matrix to be used with custom assembler
   la::PETScMatrix A = la::PETScMatrix(fem::create_matrix(*a), false);
   MatZeroEntries(A.mat());
-  dolfinx::common::Timer t1("dolfinx");
-  fem::assemble_matrix(la::PETScMatrix::set_block_fn(A.mat(), ADD_VALUES), *a, {});
+  
+  // Matrix to be used with custom DOLFINx/FFCx
+  la::PETScMatrix B = la::PETScMatrix(fem::create_matrix(*a), false);
+  MatZeroEntries(B.mat());
+
+  common::Timer t0("~Assemble Matrix Custom");
+  dolfinx::fem::impl::assemble_cells<double>(la::PETScMatrix::set_block_fn(A.mat(), ADD_VALUES),
+                                             mesh->geometry(), active_cells, dofs0, bs0, dofs1, bs1,
+                                             bc0, bc1, kernel, coeffs, constants, cell_info);
   MatAssemblyBegin(A.mat(), MAT_FINAL_ASSEMBLY);
   MatAssemblyEnd(A.mat(), MAT_FINAL_ASSEMBLY);
+  t0.stop();
+
+  common::Timer t1("~Assemble Matrix DOLINFx/FFCx");
+  dolfinx::fem::assemble_matrix(la::PETScMatrix::set_block_fn(B.mat(), ADD_VALUES), *a, {});
+  MatAssemblyBegin(B.mat(), MAT_FINAL_ASSEMBLY);
+  MatAssemblyEnd(B.mat(), MAT_FINAL_ASSEMBLY);
   t1.stop();
+
+  double normA;
+  MatNorm(A.mat(), NORM_FROBENIUS, &normA);
+
+  double normB;
+  MatNorm(A.mat(), NORM_FROBENIUS, &normB);
+
+  assert(xt::isclose(normA, normB));
 
   dolfinx::list_timings(mpi_comm, {dolfinx::TimingType::wall});
 
---
+++ cpp/kernels.hpp
@@ -0,0 +1,147 @@
+#include <basix/finite-element.h>
+#include <basix/quadrature.h>
+#include <string>
+#include <xtensor-blas/xlinalg.hpp>
+
+#include "math.hpp"
+
+constexpr std::int32_t gdim = 3;
+constexpr std::int32_t tdim = 3;
+constexpr std::int32_t d = 4;
+
+enum Kernel
+{
+  Mass,
+  Stiffness
+};
+
+using kernel_fn = std::function<void(double*, const double*, const double*, const double*,
+                                     const int*, const std::uint8_t*, const std::uint32_t)>;
+
+namespace dolfinx_cuas
+{
+
+kernel_fn generate_kernel(std::string family, std::string cell, Kernel type, int P)
+{
+
+  int quad_degree = 0;
+  if (type == Kernel::Stiffness)
+    quad_degree = (P - 1) + (P - 1);
+  else if (type == Kernel::Mass)
+    quad_degree = 2 * P;
+
+  auto [points, weight]
+      = basix::quadrature::make_quadrature("default", basix::cell::str_to_type(cell), quad_degree);
+  std::vector<double> weights(weight);
+
+  // Create Finite element for test and trial functions and tabulate shape functions
+  basix::FiniteElement element = basix::create_element(family, cell, P);
+  xt::xtensor<double, 4> basis = element.tabulate(1, points);
+  xt::xtensor<double, 2> phi = xt::view(basis, 0, xt::all(), xt::all(), 0);
+  xt::xtensor<double, 3> dphi = xt::view(basis, xt::range(1, tdim + 1), xt::all(), xt::all(), 0);
+
+  // Get coordinate element from dolfinx
+  basix::FiniteElement coordinate_element = basix::create_element("Lagrange", cell, 1);
+  xt::xtensor<double, 4> coordinate_basis = coordinate_element.tabulate(1, points);
+
+  xt::xtensor<double, 2> dphi0_c
+      = xt::round(xt::view(coordinate_basis, xt::range(1, tdim + 1), 0, xt::all(), 0));
+  std::int32_t ndofs_cell = phi.shape(1);
+
+  // Stiffness Matrix using quadrature formulation
+  // =====================================================================================
+
+  xt::xtensor<double, 3> _dphi({dphi.shape(1), dphi.shape(2), dphi.shape(0)});
+  for (std::int32_t k = 0; k < tdim; k++)
+    for (std::size_t q = 0; q < weights.size(); q++)
+      for (std::int32_t i = 0; i < ndofs_cell; i++)
+        _dphi(q, i, k) = dphi(k, q, i);
+
+  kernel_fn stiffness
+      = [dphi0_c, _dphi, phi, weights](double* A, const double* c, const double* w,
+                                       const double* coordinate_dofs, const int* entity_local_index,
+                                       const std::uint8_t* quadrature_permutation,
+                                       const std::uint32_t cell_permutation)
+  {
+    // Get geometrical data
+    xt::xtensor<double, 2> J = xt::zeros<double>({gdim, tdim});
+    xt::xtensor<double, 2> K = xt::zeros<double>({tdim, gdim});
+    std::array<std::size_t, 2> shape = {d, gdim};
+    xt::xtensor<double, 2> coord = xt::adapt(coordinate_dofs, gdim * d, xt::no_ownership(), shape);
+
+    // Compute Jacobian, its inverse and the determinant
+    dolfinx_cuas::math::compute_jacobian(dphi0_c, coord, J);
+    dolfinx_cuas::math::inv(J, K);
+    double detJ = std::fabs(dolfinx_cuas::math::det(J));
+
+    // Get number of dofs per cell
+    std::int32_t ndofs_cell = phi.shape(1);
+
+    // Main loop
+    for (std::size_t q = 0; q < weights.size(); q++)
+    {
+      double w0 = weights[q] * detJ;
+
+      // Auxiliary data structure
+      double d0[ndofs_cell];
+      double d1[ndofs_cell];
+      double d2[ndofs_cell];
+
+      // precompute J^-1 * dphi in temporary array d
+      for (int i = 0; i < ndofs_cell; i++)
+      {
+        d0[i] = K(0, 0) * _dphi(q, i, 0) + K(0, 1) * _dphi(q, i, 1) + K(0, 2) * _dphi(q, i, 2);
+        d1[i] = K(1, 0) * _dphi(q, i, 0) + K(1, 1) * _dphi(q, i, 1) + K(1, 2) * _dphi(q, i, 2);
+        d2[i] = K(2, 0) * _dphi(q, i, 0) + K(2, 1) * _dphi(q, i, 1) + K(2, 2) * _dphi(q, i, 2);
+      }
+
+      for (int i = 0; i < ndofs_cell; i++)
+      {
+        for (int j = 0; j < ndofs_cell; j++)
+        {
+          A[i * ndofs_cell + j] += (d0[i] * d0[j] + d1[i] * d1[j] + d2[i] * d2[j]) * w0;
+        }
+      }
+    }
+  };
+
+  kernel_fn mass = [=](double* A, const double* c, const double* w, const double* coordinate_dofs,
+                       const int* entity_local_index, const std::uint8_t* quadrature_permutation,
+                       const std::uint32_t cell_permutation)
+  {
+    // Get geometrical data
+    xt::xtensor<double, 2> J = xt::zeros<double>({gdim, tdim});
+    std::array<std::size_t, 2> shape = {d, gdim};
+    xt::xtensor<double, 2> coord = xt::adapt(coordinate_dofs, gdim * d, xt::no_ownership(), shape);
+
+    // Compute Jacobian, its inverse and the determinant
+    dolfinx_cuas::math::compute_jacobian(dphi0_c, coord, J);
+    double detJ = std::fabs(dolfinx_cuas::math::det(J));
+
+    // Get number of dofs per cell
+    std::int32_t ndofs_cell = phi.shape(1);
+
+    // Main loop
+    for (std::size_t q = 0; q < weights.size(); q++)
+    {
+      double w0 = weights[q] * detJ;
+      for (int i = 0; i < ndofs_cell; i++)
+      {
+        double w1 = w0 * phi.unchecked(q, i);
+        for (int j = 0; j < ndofs_cell; j++)
+          A[i * ndofs_cell + j] += w1 * phi.unchecked(q, j);
+      }
+    }
+  };
+
+  switch (type)
+  {
+  case Kernel::Mass:
+    return mass;
+  case Kernel::Stiffness:
+    return stiffness;
+  default:
+    throw std::runtime_error("unrecognized kernel");
+  }
+}
+} // namespace dolfinx_cuas
