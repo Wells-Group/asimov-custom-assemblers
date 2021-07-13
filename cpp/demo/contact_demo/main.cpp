d20b159436224855a4eff34c32e2d68d064ddc4f
Jørgen Dokken <jsd55@cam.ac.uk>
2021-07-13 12:50:09 +0000

    Move functionality from demo to stand-alone functions.

--- cpp/demo/contact_demo/main.cpp
+++ cpp/demo/contact_demo/main.cpp
@@ -5,6 +5,7 @@
 #include <dolfinx/fem/petsc.h>
 #include <dolfinx/io/XDMFFile.h>
 #include <dolfinx/mesh/utils.h>
+#include <dolfinx_cuas/assembly.hpp>
 #include <dolfinx_cuas/contact/Contact.hpp>
 #include <dolfinx_cuas/kernels.hpp>
 #include <xtensor/xio.hpp>
@@ -74,39 +75,6 @@ int main(int argc, char* argv[])
   const std::int32_t ncells = mesh->topology().index_map(tdim)->size_local();
   xt::xarray<std::int32_t> active_cells = xt::arange<std::int32_t>(0, ncells);
 
-  // Extract function space data
-  std::shared_ptr<const fem::DofMap> dofmap0 = a->function_spaces().at(0)->dofmap();
-  std::shared_ptr<const fem::DofMap> dofmap1 = a->function_spaces().at(1)->dofmap();
-  const graph::AdjacencyList<std::int32_t>& dofs0 = dofmap0->list();
-  const int bs0 = dofmap0->bs();
-  const graph::AdjacencyList<std::int32_t>& dofs1 = dofmap1->list();
-  const int bs1 = dofmap1->bs();
-  std::vector<bool> bc0;
-  std::vector<bool> bc1;
-  std::shared_ptr<const fem::FiniteElement> element0 = a->function_spaces().at(0)->element();
-  std::shared_ptr<const fem::FiniteElement> element1 = a->function_spaces().at(1)->element();
-  const std::function<void(const xtl::span<double>&, const xtl::span<const std::uint32_t>&,
-                           std::int32_t, int)>
-      apply_dof_transformation = element0->get_dof_transformation_function<double>();
-  const std::function<void(const xtl::span<double>&, const xtl::span<const std::uint32_t>&,
-                           std::int32_t, int)>
-      apply_dof_transformation_to_transpose
-      = element1->get_dof_transformation_to_transpose_function<double>();
-
-  // Pack constants and coefficients
-  const std::vector<double> constants = dolfinx::fem::pack_constants(*a);
-  const array2d<double> coeffs = dolfinx::fem::pack_coefficients(*a);
-
-  const bool needs_transformation_data = element0->needs_dof_transformations()
-                                         or element1->needs_dof_transformations()
-                                         or a->needs_facet_permutations();
-  xtl::span<const std::uint32_t> cell_info;
-  if (needs_transformation_data)
-  {
-    mesh->topology_mutable().create_entity_permutations();
-    cell_info = xtl::span(mesh->topology().get_cell_permutation_info());
-  }
-
   // auto kernel = dolfinx_cuas::generate_kernel("Lagrange", "tetrahedron",
   // Kernel::Stiffness, 1);
 
@@ -117,12 +85,9 @@ int main(int argc, char* argv[])
   // Matrix to be used with custom DOLFINx/FFCx
   la::PETScMatrix B = la::PETScMatrix(fem::create_matrix(*a), false);
   MatZeroEntries(B.mat());
-  const std::vector<std::uint8_t>& perms = mesh->topology().get_facet_permutations();
   common::Timer t0("~Assemble Matrix Custom");
-  dolfinx::fem::impl::assemble_exterior_facets<double>(
-      la::PETScMatrix::set_block_fn(A.mat(), ADD_VALUES), *mesh, left_facets,
-      apply_dof_transformation, dofs0, bs0, apply_dof_transformation_to_transpose, dofs1, bs1, bc0,
-      bc1, kernel, coeffs, constants, cell_info, perms);
+  dolfinx_cuas::assemble_exterior_facets(la::PETScMatrix::set_block_fn(A.mat(), ADD_VALUES), a,
+                                         left_facets, kernel);
   MatAssemblyBegin(A.mat(), MAT_FINAL_ASSEMBLY);
   MatAssemblyEnd(A.mat(), MAT_FINAL_ASSEMBLY);
   t0.stop();
