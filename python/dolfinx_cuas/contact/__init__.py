012376e0ab92647342fdb6e78ceb3e665df80f71
Sarah Roggendorf <33656497+SarahRo@users.noreply.github.com>
2021-10-01 09:35:53 +0100

    remove contact related code (#78)
    * remove contact related code
    
    * fix flake8

--- python/dolfinx_cuas/contact/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (C) 2021 Jørgen S. Dokken
-#
-# SPDX-License-Identifier:    LGPL-3.0-or-later
-
-"""Contact specific algorithms for custom assemblers"""
-
-# flake8: noqa
-
-from .build_gap_function import facet_master_puppet_relation
