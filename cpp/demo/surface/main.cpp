66993ae0f82b5741cbc8d778e1b77bf9b2855cd4
Jørgen Dokken <jsd55@cam.ac.uk>
2022-03-28 15:05:44 +0000

    Fix C++ demos

--- cpp/demo/surface/main.cpp
+++ cpp/demo/surface/main.cpp
@@ -20,82 +20,85 @@ using namespace dolfinx;
 
 int main(int argc, char* argv[])
 {
-  common::subsystem::init_logging(argc, argv);
-  common::subsystem::init_petsc(argc, argv);
+  dolfinx::init_logging(argc, argv);
 
-  MPI_Comm mpi_comm{MPI_COMM_WORLD};
+  PetscInitialize(&argc, &argv, nullptr, nullptr);
+  {
+    MPI_Comm mpi_comm{MPI_COMM_WORLD};
 
-  const int N = 5;
-  std::shared_ptr<mesh::Mesh> mesh = std::make_shared<mesh::Mesh>(
-      mesh::create_box(mpi_comm, {{{0.0, 0.0, 0.0}, {1.0, 1.0, 1.0}}}, {N, N, N},
-                       mesh::CellType::tetrahedron, mesh::GhostMode::none));
-  mesh->topology().create_entity_permutations();
-  // Locate boundary facets with x=0
-  auto boundary = [](auto& x) -> xt::xtensor<bool, 1> { return xt::isclose(xt::row(x, 0), 0.0); };
-  std::vector<std::int32_t> boundary_facets
-      = dolfinx::mesh::locate_entities_boundary(*mesh, 2, boundary);
-  dolfinx::radix_sort(xtl::span(boundary_facets));
+    const int N = 5;
+    std::shared_ptr<mesh::Mesh> mesh = std::make_shared<mesh::Mesh>(
+        mesh::create_box(mpi_comm, {{{0.0, 0.0, 0.0}, {1.0, 1.0, 1.0}}}, {N, N, N},
+                         mesh::CellType::tetrahedron, mesh::GhostMode::none));
+    mesh->topology().create_entity_permutations();
+    // Locate boundary facets with x=0
+    auto boundary = [](auto& x) -> xt::xtensor<bool, 1> { return xt::isclose(xt::row(x, 0), 0.0); };
+    std::vector<std::int32_t> boundary_facets
+        = dolfinx::mesh::locate_entities_boundary(*mesh, 2, boundary);
+    dolfinx::radix_sort(xtl::span(boundary_facets));
 
-  std::vector<std::int32_t> boundary_values(boundary_facets.size());
-  std::fill(boundary_values.begin(), boundary_values.end(), 1);
-  auto mt = std::make_shared<dolfinx::mesh::MeshTags<std::int32_t>>(mesh, 2, boundary_facets,
-                                                                    boundary_values);
+    std::vector<std::int32_t> boundary_values(boundary_facets.size());
+    std::fill(boundary_values.begin(), boundary_values.end(), 1);
+    auto mt = std::make_shared<dolfinx::mesh::MeshTags<std::int32_t>>(mesh, 2, boundary_facets,
+                                                                      boundary_values);
 
-  // Generate function space
-  const int Q = 1; // Degree of function space
-  auto V = std::make_shared<fem::FunctionSpace>(
-      fem::create_functionspace(functionspace_form_problem_a, "u", mesh));
+    // Generate function space
+    const int Q = 1; // Degree of function space
+    auto V = std::make_shared<fem::FunctionSpace>(
+        fem::create_functionspace(functionspace_form_problem_a, "u", mesh));
 
-  // Generate boundary kernel
-  const std::int32_t tdim = mesh->topology().dim();
+    // Generate boundary kernel
+    const std::int32_t tdim = mesh->topology().dim();
 
-  dolfinx_cuas::QuadratureRule q_rule(mesh->topology().cell_type(), 2 * (Q - 1), tdim - 1);
-  auto kernel = dolfinx_cuas::generate_surface_kernel<PetscScalar>(V, dolfinx_cuas::Kernel::SymGrad,
-                                                                   q_rule);
+    dolfinx_cuas::QuadratureRule q_rule(mesh->topology().cell_type(), 2 * (Q - 1), tdim - 1);
+    auto kernel = dolfinx_cuas::generate_surface_kernel<PetscScalar>(
+        V, dolfinx_cuas::Kernel::SymGrad, q_rule);
 
-  // Define variational forms
-  auto kappa = std::make_shared<fem::Constant<PetscScalar>>(1.0);
-  auto a = std::make_shared<fem::Form<PetscScalar>>(
-      fem::create_form<PetscScalar>(*form_problem_a, {V, V}, {}, {{"kappa", kappa}},
-                                    {{dolfinx::fem::IntegralType::exterior_facet, &(*mt)}}));
+    // Define variational forms
+    auto kappa = std::make_shared<fem::Constant<PetscScalar>>(1.0);
+    auto a = std::make_shared<fem::Form<PetscScalar>>(
+        fem::create_form<PetscScalar>(*form_problem_a, {V, V}, {}, {{"kappa", kappa}},
+                                      {{dolfinx::fem::IntegralType::exterior_facet, &(*mt)}}));
 
-  // Define active cells
-  const std::int32_t ncells = mesh->topology().index_map(tdim)->size_local();
-  xt::xarray<std::int32_t> active_cells = xt::arange<std::int32_t>(0, ncells);
+    // Define active cells
+    const std::int32_t ncells = mesh->topology().index_map(tdim)->size_local();
+    xt::xarray<std::int32_t> active_cells = xt::arange<std::int32_t>(0, ncells);
 
-  // Matrix to be used with custom assembler
-  la::petsc::Matrix A = la::petsc::Matrix(fem::petsc::create_matrix(*a), false);
-  MatZeroEntries(A.mat());
+    // Matrix to be used with custom assembler
+    la::petsc::Matrix A = la::petsc::Matrix(fem::petsc::create_matrix(*a), false);
+    MatZeroEntries(A.mat());
 
-  // Matrix to be used with custom DOLFINx/FFCx
-  la::petsc::Matrix B = la::petsc::Matrix(fem::petsc::create_matrix(*a), false);
-  MatZeroEntries(B.mat());
-  common::Timer t0("~Assemble Matrix Custom");
-  const std::vector<PetscScalar> coeffs(ncells * 0);
-  const std::vector<PetscScalar> consts(0);
-  dolfinx_cuas::assemble_matrix<PetscScalar>(la::petsc::Matrix::set_block_fn(A.mat(), ADD_VALUES),
-                                             V, {}, boundary_facets, kernel, coeffs, 0, consts,
-                                             dolfinx::fem::IntegralType::exterior_facet);
-  MatAssemblyBegin(A.mat(), MAT_FINAL_ASSEMBLY);
-  MatAssemblyEnd(A.mat(), MAT_FINAL_ASSEMBLY);
-  t0.stop();
+    // Matrix to be used with custom DOLFINx/FFCx
+    la::petsc::Matrix B = la::petsc::Matrix(fem::petsc::create_matrix(*a), false);
+    MatZeroEntries(B.mat());
+    common::Timer t0("~Assemble Matrix Custom");
+    const std::vector<PetscScalar> coeffs(ncells * 0);
+    const std::vector<PetscScalar> consts(0);
+    dolfinx_cuas::assemble_matrix<PetscScalar>(la::petsc::Matrix::set_block_fn(A.mat(), ADD_VALUES),
+                                               V, {}, boundary_facets, kernel, coeffs, 0, consts,
+                                               dolfinx::fem::IntegralType::exterior_facet);
+    MatAssemblyBegin(A.mat(), MAT_FINAL_ASSEMBLY);
+    MatAssemblyEnd(A.mat(), MAT_FINAL_ASSEMBLY);
+    t0.stop();
 
-  common::Timer t1("~Assemble Matrix DOLINFx/FFCx");
-  dolfinx::fem::assemble_matrix(la::petsc::Matrix::set_block_fn(B.mat(), ADD_VALUES), *a, {});
-  MatAssemblyBegin(B.mat(), MAT_FINAL_ASSEMBLY);
-  MatAssemblyEnd(B.mat(), MAT_FINAL_ASSEMBLY);
-  t1.stop();
+    common::Timer t1("~Assemble Matrix DOLINFx/FFCx");
+    dolfinx::fem::assemble_matrix(la::petsc::Matrix::set_block_fn(B.mat(), ADD_VALUES), *a, {});
+    MatAssemblyBegin(B.mat(), MAT_FINAL_ASSEMBLY);
+    MatAssemblyEnd(B.mat(), MAT_FINAL_ASSEMBLY);
+    t1.stop();
 
-  double normA;
-  MatNorm(A.mat(), NORM_FROBENIUS, &normA);
+    double normA;
+    MatNorm(A.mat(), NORM_FROBENIUS, &normA);
 
-  double normB;
-  MatNorm(B.mat(), NORM_FROBENIUS, &normB);
-  assert(xt::isclose(normA, normB));
-  if (!dolfinx_cuas::allclose(A.mat(), B.mat()))
-    throw std::runtime_error("Matrices are not the same");
+    double normB;
+    MatNorm(B.mat(), NORM_FROBENIUS, &normB);
+    assert(xt::isclose(normA, normB));
+    if (!dolfinx_cuas::allclose(A.mat(), B.mat()))
+      throw std::runtime_error("Matrices are not the same");
 
-  dolfinx::list_timings(mpi_comm, {dolfinx::TimingType::wall});
+    dolfinx::list_timings(mpi_comm, {dolfinx::TimingType::wall});
+  }
+  PetscFinalize();
 
   return 0;
 }
--- cpp/demo/volume/main.cpp
+++ cpp/demo/volume/main.cpp
@@ -21,8 +21,6 @@ namespace po = boost::program_options;
 
 int main(int argc, char* argv[])
 {
-  common::subsystem::init_logging(argc, argv);
-  common::subsystem::init_petsc(argc, argv);
 
   po::options_description desc("Allowed options");
   desc.add_options()("help,h", "print usage message")(
@@ -37,106 +35,110 @@ int main(int argc, char* argv[])
     std::cout << desc << "\n";
     return 0;
   }
-  const std::string problem_type = vm["kernel"].as<std::string>();
-  const int degree = vm["degree"].as<int>();
-
-  MPI_Comm mpi_comm{MPI_COMM_WORLD};
-
-  std::shared_ptr<mesh::Mesh> mesh = std::make_shared<mesh::Mesh>(
-      mesh::create_box(mpi_comm, {{{0.0, 0.0, 0.0}, {1.0, 1.0, 1.0}}}, {10, 10, 10},
-                       mesh::CellType::tetrahedron, mesh::GhostMode::none));
-
-  mesh->topology().create_entity_permutations();
-
-  auto kappa = std::make_shared<fem::Constant<PetscScalar>>(1.0);
-
-  // Define variational forms
-  ufcx_form form;
-  std::shared_ptr<fem::FunctionSpace> V;
-  dolfinx_cuas::Kernel kernel_type;
-
-  int q_degree = 0;
-  if (problem_type == "mass")
+  dolfinx::init_logging(argc, argv);
+  PetscInitialize(&argc, &argv, nullptr, nullptr);
   {
-    q_degree = 2 * degree;
-    kernel_type = dolfinx_cuas::Kernel::Mass;
-    std::vector spaces_mass = {functionspace_form_volume_a_mass1, functionspace_form_volume_a_mass2,
-                               functionspace_form_volume_a_mass3, functionspace_form_volume_a_mass4,
-                               functionspace_form_volume_a_mass5};
-    std::vector forms_mass = {form_volume_a_mass1, form_volume_a_mass2, form_volume_a_mass3,
-                              form_volume_a_mass4, form_volume_a_mass5};
-    V = std::make_shared<fem::FunctionSpace>(
-        fem::create_functionspace(spaces_mass[degree - 1], "v_0", mesh));
-    form = *forms_mass[degree - 1];
+    const std::string problem_type = vm["kernel"].as<std::string>();
+    const int degree = vm["degree"].as<int>();
+
+    MPI_Comm mpi_comm{MPI_COMM_WORLD};
+    std::shared_ptr<mesh::Mesh> mesh = std::make_shared<mesh::Mesh>(
+        mesh::create_box(mpi_comm, {{{0.0, 0.0, 0.0}, {1.0, 1.0, 1.0}}}, {10, 10, 10},
+                         mesh::CellType::tetrahedron, mesh::GhostMode::none));
+
+    mesh->topology().create_entity_permutations();
+
+    auto kappa = std::make_shared<fem::Constant<PetscScalar>>(1.0);
+
+    // Define variational forms
+    ufcx_form form;
+    std::shared_ptr<fem::FunctionSpace> V;
+    dolfinx_cuas::Kernel kernel_type;
+
+    int q_degree = 0;
+    if (problem_type == "mass")
+    {
+      q_degree = 2 * degree;
+      kernel_type = dolfinx_cuas::Kernel::Mass;
+      std::vector spaces_mass
+          = {functionspace_form_volume_a_mass1, functionspace_form_volume_a_mass2,
+             functionspace_form_volume_a_mass3, functionspace_form_volume_a_mass4,
+             functionspace_form_volume_a_mass5};
+      std::vector forms_mass = {form_volume_a_mass1, form_volume_a_mass2, form_volume_a_mass3,
+                                form_volume_a_mass4, form_volume_a_mass5};
+      V = std::make_shared<fem::FunctionSpace>(
+          fem::create_functionspace(spaces_mass[degree - 1], "v_0", mesh));
+      form = *forms_mass[degree - 1];
+    }
+    else if (problem_type == "stiffness")
+    {
+      q_degree = 2 * (degree - 1);
+      kernel_type = dolfinx_cuas::Kernel::Stiffness;
+      std::vector spaces_stiffness
+          = {functionspace_form_volume_a_stiffness1, functionspace_form_volume_a_stiffness2,
+             functionspace_form_volume_a_stiffness3, functionspace_form_volume_a_stiffness4,
+             functionspace_form_volume_a_stiffness5};
+      std::vector forms_stiffness
+          = {form_volume_a_stiffness1, form_volume_a_stiffness2, form_volume_a_stiffness3,
+             form_volume_a_stiffness4, form_volume_a_stiffness5};
+      V = std::make_shared<fem::FunctionSpace>(
+          fem::create_functionspace(spaces_stiffness[degree - 1], "v_0", mesh));
+      form = *forms_stiffness[degree - 1];
+    }
+    else
+      throw std::runtime_error("Unsupported kernel");
+
+    auto a = std::make_shared<fem::Form<PetscScalar>>(
+        fem::create_form<PetscScalar>(form, {V, V}, {}, {{"kappa", kappa}}, {}));
+
+    // Matrix to be used with custom assembler
+    la::petsc::Matrix A = la::petsc::Matrix(fem::petsc::create_matrix(*a), false);
+    MatZeroEntries(A.mat());
+
+    // Matrix to be used with custom DOLFINx/FFCx
+    la::petsc::Matrix B = la::petsc::Matrix(fem::petsc::create_matrix(*a), false);
+    MatZeroEntries(B.mat());
+
+    // Generate Kernel
+    dolfinx_cuas::QuadratureRule q_rule(mesh->topology().cell_type(), q_degree,
+                                        mesh->topology().dim(), basix::quadrature::type::Default);
+    auto kernel = dolfinx_cuas::generate_kernel<PetscScalar>(kernel_type, degree,
+                                                             V->dofmap()->index_map_bs(), q_rule);
+
+    // Define active cells
+    const std::int32_t tdim = mesh->topology().dim();
+    const std::int32_t ncells = mesh->topology().index_map(tdim)->size_local();
+    xt::xarray<std::int32_t> active_cells = xt::arange<std::int32_t>(0, ncells);
+    const std::vector<PetscScalar> coeffs(0);
+    const std::vector<PetscScalar> consts(0);
+
+    common::Timer t0("~Assemble Matrix Custom");
+    dolfinx_cuas::assemble_matrix<PetscScalar>(la::petsc::Matrix::set_block_fn(A.mat(), ADD_VALUES),
+                                               V, {}, active_cells, kernel, coeffs, 0, consts,
+                                               dolfinx::fem::IntegralType::cell);
+    MatAssemblyBegin(A.mat(), MAT_FINAL_ASSEMBLY);
+    MatAssemblyEnd(A.mat(), MAT_FINAL_ASSEMBLY);
+    t0.stop();
+
+    {
+      // Prepare constants and coefficients
+      const auto constants = pack_constants(*a);
+      auto coeffs = fem::allocate_coefficient_storage(*a);
+      pack_coefficients(*a, coeffs);
+      common::Timer t1("~Assemble Matrix DOLINFx/FFCx");
+      dolfinx::fem::assemble_matrix(la::petsc::Matrix::set_block_fn(B.mat(), ADD_VALUES), *a,
+                                    tcb::make_span(constants),
+                                    dolfinx::fem::make_coefficients_span(coeffs), {});
+      MatAssemblyBegin(B.mat(), MAT_FINAL_ASSEMBLY);
+      MatAssemblyEnd(B.mat(), MAT_FINAL_ASSEMBLY);
+      t1.stop();
+    }
+
+    dolfinx::list_timings(mpi_comm, {dolfinx::TimingType::wall});
+
+    if (!dolfinx_cuas::allclose(A.mat(), B.mat()))
+      throw std::runtime_error("Matrices are not the same");
   }
-  else if (problem_type == "stiffness")
-  {
-    q_degree = 2 * (degree - 1);
-    kernel_type = dolfinx_cuas::Kernel::Stiffness;
-    std::vector spaces_stiffness
-        = {functionspace_form_volume_a_stiffness1, functionspace_form_volume_a_stiffness2,
-           functionspace_form_volume_a_stiffness3, functionspace_form_volume_a_stiffness4,
-           functionspace_form_volume_a_stiffness5};
-    std::vector forms_stiffness
-        = {form_volume_a_stiffness1, form_volume_a_stiffness2, form_volume_a_stiffness3,
-           form_volume_a_stiffness4, form_volume_a_stiffness5};
-    V = std::make_shared<fem::FunctionSpace>(
-        fem::create_functionspace(spaces_stiffness[degree - 1], "v_0", mesh));
-    form = *forms_stiffness[degree - 1];
-  }
-  else
-    throw std::runtime_error("Unsupported kernel");
-
-  auto a = std::make_shared<fem::Form<PetscScalar>>(
-      fem::create_form<PetscScalar>(form, {V, V}, {}, {{"kappa", kappa}}, {}));
-
-  // Matrix to be used with custom assembler
-  la::petsc::Matrix A = la::petsc::Matrix(fem::petsc::create_matrix(*a), false);
-  MatZeroEntries(A.mat());
-
-  // Matrix to be used with custom DOLFINx/FFCx
-  la::petsc::Matrix B = la::petsc::Matrix(fem::petsc::create_matrix(*a), false);
-  MatZeroEntries(B.mat());
-
-  // Generate Kernel
-  dolfinx_cuas::QuadratureRule q_rule(mesh->topology().cell_type(), q_degree,
-                                      mesh->topology().dim(), basix::quadrature::type::Default);
-  auto kernel = dolfinx_cuas::generate_kernel<PetscScalar>(kernel_type, degree,
-                                                           V->dofmap()->index_map_bs(), q_rule);
-
-  // Define active cells
-  const std::int32_t tdim = mesh->topology().dim();
-  const std::int32_t ncells = mesh->topology().index_map(tdim)->size_local();
-  xt::xarray<std::int32_t> active_cells = xt::arange<std::int32_t>(0, ncells);
-  const std::vector<PetscScalar> coeffs(0);
-  const std::vector<PetscScalar> consts(0);
-
-  common::Timer t0("~Assemble Matrix Custom");
-  dolfinx_cuas::assemble_matrix<PetscScalar>(la::petsc::Matrix::set_block_fn(A.mat(), ADD_VALUES),
-                                             V, {}, active_cells, kernel, coeffs, 0, consts,
-                                             dolfinx::fem::IntegralType::cell);
-  MatAssemblyBegin(A.mat(), MAT_FINAL_ASSEMBLY);
-  MatAssemblyEnd(A.mat(), MAT_FINAL_ASSEMBLY);
-  t0.stop();
-
-  {
-    // Prepare constants and coefficients
-    const auto constants = pack_constants(*a);
-    auto coeffs = fem::allocate_coefficient_storage(*a);
-    pack_coefficients(*a, coeffs);
-    common::Timer t1("~Assemble Matrix DOLINFx/FFCx");
-    dolfinx::fem::assemble_matrix(la::petsc::Matrix::set_block_fn(B.mat(), ADD_VALUES), *a,
-                                  tcb::make_span(constants),
-                                  dolfinx::fem::make_coefficients_span(coeffs), {});
-    MatAssemblyBegin(B.mat(), MAT_FINAL_ASSEMBLY);
-    MatAssemblyEnd(B.mat(), MAT_FINAL_ASSEMBLY);
-    t1.stop();
-  }
-
-  dolfinx::list_timings(mpi_comm, {dolfinx::TimingType::wall});
-
-  if (!dolfinx_cuas::allclose(A.mat(), B.mat()))
-    throw std::runtime_error("Matrices are not the same");
-
+  PetscFinalize();
   return 0;
 }
